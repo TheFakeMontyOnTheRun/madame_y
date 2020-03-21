1b58a56ba94cec78cba27300752a4e5a112e7218
Daniel Monteiro <danielmonteiro@id.uff.br>
2020-03-21 15:01:43 +0000

    Beurette graduated into madame
    Seriously, some people might take offence with this term. Better to change it to something harmless.

--- cfg/build_config.mk
+++ cfg/build_config.mk
@@ -48,7 +48,7 @@
 
 # Name of the project (without spaces, as it will be used as filename)
 #   and Z80 memory location where code will start in the generated binary
-PROJNAME   := beurette
+PROJNAME   := game
 Z80CODELOC := 0x4000
 
 ##
